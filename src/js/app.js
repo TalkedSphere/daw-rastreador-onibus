fa4418e93a5861caa9ad96c6d0d965044d5b3342
TalkedSphere <fredericosantos0309@gmail.com>
2026-04-28 10:22:47 -0300

    App.js changes

---
+++ src/js/app.js
@@ -0,0 +1,23 @@
+// CONSTANTES \\
+const urlAPI = "https://corsproxy.io/?url=https://temporeal.pbh.gov.br/?param=D"
+let posUser = null;
+
+// FUNÇÕES \\
+function iniciar() {
+    // Verifica se o navegador apresenta GPS e pega as coordenadas oferecidas por ele.
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(
+            (pos) => posUser = {lat: pos.coords.latitude, lon: pos.coords.longitude}),
+            () => console.log("Incapaz de localizar o usuário.");
+    }
+}
+
+// Função para buscar dados e atualizar marcadores
+async function atualizarOnibus() {
+    fetch(urlAPI)
+        .then(response => response.json())
+        .then(_data => {
+            // Lógica para limpar marcadores antigos e adicionar novos
+            console.log("Dados atualizados!");
+        });
+}
